fb692679ea8162d3a629649aa5aa073de8b58ac8
城北吕公 <878636660@qq.com>
2024-06-22 21:06:24 +0800

    实现多线程同步下载图片

--- thread-demo/src/com/lvchenglong/demo1/TestThread2.java
+++ thread-demo/src/com/lvchenglong/demo1/TestThread2.java
@@ -1,2 +1,53 @@
-package com.lvchenglong.demo1;public class TestThread2 {
+package com.lvchenglong.demo1;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+// 实现多线程同步下载图片
+public class TestThread2 extends Thread{
+    private String url;  // 网络图片地址
+    private String name;  // 保存的文件名
+
+    // 构造器
+    public TestThread2(String url,String name){
+        this.url = url;
+        this.name = name;
+    }
+
+    // 下载图片线程的执行体
+    @Override
+    public void run(){
+        WebDownLoader webDownLoader = new WebDownLoader();
+        try {
+            webDownLoader.downloader(url,name);
+            System.out.println("下载了文件名为" + name);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args){
+        TestThread2 t1 = new TestThread2("https://dongping-blog.oss-cn-hangzhou.aliyuncs.com/systemImage/1cb3d9ad7ec64d45895bde132002a0ed.png","1.png");
+        TestThread2 t2 = new TestThread2("https://dongping-blog.oss-cn-hangzhou.aliyuncs.com/maxEmoticon/606a01291ba74a219bdeb6a34ef4175d.png","2.png");
+        TestThread2 t3 = new TestThread2("https://dongping-blog.oss-cn-hangzhou.aliyuncs.com/appImage/5adec1eb48a64214a2f8282facf26abf.png","3.png");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+// 下载器
+class WebDownLoader{
+    // 下载方法
+    public void downloader(String url,String name) throws IOException {
+        try {
+            FileUtils.copyURLToFile(new URL(url),new File(name));
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("IO异常，downloader方法出现问题");
+        }
+    }
 }
